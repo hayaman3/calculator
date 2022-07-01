8d2b2eecd7e37b12bef65578b4dd02e78c31004d
Kevin <kevin.haya@yahoo.com>
2022-07-01 23:58:08 +0800

    Fixed problem with parseFloat returning strings

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ const allClear = document.getElementById("all-clear")
 const equal = document.getElementById("equal")
 const operator = document.querySelectorAll(".operator")
 
-let num = [];
+let numbersArray = [];
+let operatorsArray = [];
 let temp = "";
-let operatorClicked = false;
-let lastOperator = "";
+let operatorClicked = true;
+let lastOperator = "none";
 
 allClear.addEventListener('click',clearScreen)
 equal.addEventListener('click' , operate)
@@ -20,23 +21,25 @@ numbers.forEach((button, i) => {
         displayCurrent.innerText += numbers[i].innerText;;
 
         temp = parseFloat(displayCurrent.innerText);
-        
-        operatorClicked = true;
 
-        if(!operatorClicked){
-            num.push(lastOperator)
+
+        if(operatorClicked&&lastOperator!="none"){
+            operatorsArray.push(lastOperator)
         }
+
+        operatorClicked = false;
+
 
     });
 });
 
 operator.forEach((button, i) => {
     button.addEventListener("click", () => {
-        if(operatorClicked){
+        if(!operatorClicked){
 
-            operatorClicked = false;
+            operatorClicked = true;
 
-            num.push(temp);
+            numbersArray.push(temp);
 
             lastOperator = operator[i].innerText;
             displayPast.innerText += displayCurrent.innerText;
@@ -45,10 +48,41 @@ operator.forEach((button, i) => {
     });
 });
 
+let ans = +"";
+
 function operate(){
-    if (num.length==0){
-        displayCurrent.innerText = "0"
+    //check to push last number
+    if(!(displayCurrent.innerHTML=="")){
+        numbersArray.push(parseFloat(displayCurrent.innerHTML));
     }
+
+    //check numbers array for length
+    if(numbersArray.length==0){
+        ans = 0
+    }
+    if(numbersArray.length==1){
+        ans = numbersArray[0]
+
+    }else{
+        for (let i = 0; i <= operatorsArray.length;i++){
+
+            numi = Number(numbersArray[i]);
+            numi1 = Number(numbersArray[i+1]);
+            let placeHolder = +""
+
+            if(operatorsArray[i]=="+"){
+
+                placeHolder = numi + numi1;
+                ans = placeHolder + Number(ans);
+                console.log(ans)
+
+                
+            }
+        }
+    }
+
+    //ans = numbersArray[0]+numbersArray[1]
+    //console.log(ans)
 
 }
 
@@ -56,5 +90,5 @@ function clearScreen(){
     displayCurrent.innerText = "";
     displayPast.innerText = "";
     num = [];
+    operatorClicked = false;
 }
-     
