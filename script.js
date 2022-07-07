cc0f8e92d9864bc98690974b6abb56a76c4b998b
Kevin <kevin.haya@yahoo.com>
2022-07-07 23:18:58 +0800

    Added css animation and renamed variables

--- script.js
+++ script.js
@@ -2,134 +2,219 @@ let numbersArray = [];
 let operatorsArray = [];
 let currentIndex = 0;
 
-const numbers = document.querySelectorAll(".number");
+//displays
 const displayCurrent = document.getElementById("display-current");
 const displayPast = document.getElementById("display-past");
-const main = document.getElementById("main")
-
+//buttons
 const allClear = document.getElementById("all-clear");
 const equal = document.getElementById("equal");
+const numbers = document.querySelectorAll(".number");
 const operator = document.querySelectorAll(".operator");
 
-allClear.addEventListener('click',clearScreen);
-equal.addEventListener('click' , evaluate);
+const glitch = document.getElementById("glitch")
+
+//button functions
+allClear.addEventListener('click',allClearFunc);
+equal.addEventListener('click' , equalFunc);
 
 numbers.forEach((button, i) => {
     button.addEventListener("click", () => {
         let input = numbers[i].textContent;
-        if(viableDisplaySize() && periodExists(input)){
+        if(fitsDisplay() && !hasDecimal(input)){
             updateDisplay(input,"number");
             updateNumbersArray(input);
-        }else{
-
         }
-    });
-});
-
-function periodExists(button){
-    if(button=="."){
-        if((/[.]/.test(displayCurrent.textContent))){
-            return false;
-        }else{
-            return true;
-        }
-    }else{
-        return true;
-    }
-}
+    })
+})
 
 operator.forEach((button, i) => {
     button.addEventListener("click", () => {
         let input = operator[i].textContent;
-        if(viableDisplaySize() && preventRepeatedOperator()){
+        if(fitsDisplay() && !hasRepeatOperators()){
             updateDisplay(input,"operator");
             updateOperatorsArray(input);
-        }else{
-            
         }
-    });
-});
+    })
+})
 
+function equalFunc(){
+    if(!recentlySolved("=")){
+        let ans = 0;
+        updateArrays();
+        if(numbersArray.length == 0){
+            return;
+        }else if(numbersArray.length == 1 ){
+            ans = numbersArray[0];
+        }else{
+            ans = evaluate();
+        }
+        updateDisplay(ans,"equal");
+        saveAns(ans);
+    }
+}
+
+function evaluate(){
+    let nextNumber = Number(numbersArray[0]);
+    ans = nextNumber;
+    for (let i = 1, j = 0; j < operatorsArray.length; i++, j++){
+        nextNumber =  Number(numbersArray[i]);
+        switch(operatorsArray[j]){
+            case "+":
+                ans  += nextNumber;
+                break;
+            case "-":
+                ans -= nextNumber;
+                break;
+            case "x":
+                ans *= nextNumber;
+                break;
+            case "÷":
+                if(nextNumber==0){
+                    ans = 0;
+                    updateDisplay(ans,"didivdedByZero");
+                    return
+                }else{
+                    ans /= nextNumber;
+                    break;
+                }
+            default:
+                // do nothing
+                console.log("something is wrong in switch(operator)");
+                break;
+        }
+    }
+    return ans;
+}
+
+function allClearFunc(){
+    displayCurrent.textContent = "";
+    displayPast.textContent = "";
+    numbersArray = [];
+    operatorsArray = [];
+    currentIndex = 0;
+    glitchAnimation(false);
+}
+
+function saveAns(ans){
+    numbersArray = [];
+    currentIndex = 0;
+    numbersArray[0] = ans;
+    operatorsArray = [];
+}
+
+//update functions
 function updateNumbersArray(data){
     if(numbersArray[currentIndex]===undefined){
-        numbersArray[currentIndex] = data
+        numbersArray[currentIndex] = data;
     }else{
-        numbersArray[currentIndex] += data
+        numbersArray[currentIndex] += data;
     }
 }
 
 function updateOperatorsArray(operatorSign){
     currentIndex ++;
-    operatorsArray.push(operatorSign)
+    operatorsArray.push(operatorSign);
 }
 
-function viableDisplaySize(){
+function updateDisplay(input,buttonClicked){
+    if(recentlySolved()){
+        displayPast.textContent = "";
+    }
+    switch(buttonClicked){
+        case "number":
+            if(!(displayCurrent.textContent[0]==0)){
+                displayCurrent.textContent += input;
+            }else{
+                displayCurrent.textContent = input;
+            }
+            break;
+        case "operator":
+            displayPast.textContent += displayCurrent.textContent;
+            displayPast.textContent += input;
+            displayCurrent.textContent = "";
+            break;
+        case "equal":
+            if(hasExtraOperator()){
+                displayPast.textContent = displayPast.textContent.slice(0,-1);
+                displayPast.textContent += displayCurrent.textContent;
+                displayPast.textContent +=  "=";
+                displayCurrent.textContent = input;
+            }else{
+                displayPast.textContent += displayCurrent.textContent;
+                displayPast.textContent +=  "=";
+                displayCurrent.textContent = input;
+            }
+            break;
+        case "didivdedByZero":
+            glitchAnimation(true);
+            break;
+        default:
+            // do nothing
+            console.log("something is wrong in display update");
+            break;    
+    }
+}
+
+function hasExtraOperator(){
+    let lastIndex = displayPast.textContent.length-1;
+    if((/[+\-\x\÷]/.test(displayPast.textContent[lastIndex]))&&displayCurrent.textContent.length==0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function updateArrays(){
+    if(numbersArray.length <= operatorsArray.length){
+        operatorsArray.pop();
+    }
+    numbersArray = numbersArray.map(stringToNumber =>{
+        return Number(stringToNumber)
+    });
+}
+
+// boolean checks
+function fitsDisplay(){
     if(displayCurrent.textContent.length>12){
-        shake(displayCurrent)
+        shakeAnimation(displayCurrent);
         return false;
     }else if(displayPast.textContent.length>17){
-        shake(displayPast)
+        shakeAnimation(displayPast);
         return false;
     }else if(displayCurrent.textContent.length+displayPast.textContent.length>17){
-        shake(displayCurrent)
-        shake(displayPast)
+        shakeAnimation(displayCurrent);
+        shakeAnimation(displayPast);
         return false;
     }else{
         return true;
     }
 }
 
-function preventRepeatedOperator(){
+function hasRepeatOperators(){
     let currentDisplayLength = displayCurrent.textContent.length;
     let numbersArrayLength = numbersArray.length;
 
     if(numbersArrayLength>0 && currentDisplayLength>0){
-        return true;
+        return false;
     }else{
-        shake(displayPast)
+        shakeAnimation(displayPast);
+        return true;
+    }
+}
+
+function hasDecimal(button){
+    if(button=="."){
+        if((/[.]/.test(displayCurrent.textContent))){
+            return true;
+        }else{
+            return false;
+        }
+    }else{
         return false;
     }
 }
 
-function shake(element){
-    element.classList.remove("shake");
-    element.offsetWidth;
-    element.classList.add("shake");
-}
-
-function updateDisplay(input,buttonClicked){
-    if(previouslyEvaluated()){
-        displayPast.textContent = "";
-    }
-    if(buttonClicked==="number"){
-        if(!(displayCurrent.textContent[0]==0)){
-            displayCurrent.textContent += input;
-        }else{
-            displayCurrent.textContent = input;
-        }
-    }else if(buttonClicked==="operator"){
-        displayPast.textContent += displayCurrent.textContent;
-        displayPast.textContent += input
-        displayCurrent.textContent = "";
-    }else if(buttonClicked==="equal"){
-        let lastIndex = displayPast.textContent.length-1;
-        if((/[+\-\x\÷]/.test(displayPast.textContent[lastIndex]))&&displayCurrent.textContent.length==0){
-            displayPast.textContent = displayPast.textContent.slice(0,-1)
-            displayPast.textContent += displayCurrent.textContent
-            displayPast.textContent +=  "="
-            displayCurrent.textContent = input;
-        }else{
-            displayPast.textContent += displayCurrent.textContent
-            displayPast.textContent +=  "="
-            displayCurrent.textContent = input;
-        }
-    }
-    else{
-        console.log("did  not updateDisplay")
-    }
-}
-
-function previouslyEvaluated(){
+function recentlySolved(){
     let lastIndex = displayPast.textContent.length-1;
     if(displayPast.textContent[lastIndex] == "="){
         return true;
@@ -138,80 +223,23 @@ function previouslyEvaluated(){
     }
 }
 
+// animation
+function shakeAnimation(element){
+    element.classList.remove("shake");
+    element.offsetWidth;
+    element.classList.add("shake");
+}
 
-
-function evaluate(){
-    if(!previouslyEvaluated("=")){
-        let ans = 0;
-        updateArrays()
-        if(numbersArray.length == 0){
-            return;
-        }else if(numbersArray.length == 1 ){
-            ans = numbersArray[0];
-        }else{
-            let placeHolder = Number(numbersArray[0]);
-            ans = placeHolder;
-
-            for (let i = 1, j = 0; j <= operatorsArray.length; i++, j++){
-                placeHolder =  Number(numbersArray[i]);
-                switch(operatorsArray[j]){
-                    case "+":
-                        ans  += placeHolder;
-                        break;
-                    case "-":
-                        ans -= placeHolder;
-                        break;
-                    case "x":
-                        ans *= placeHolder;
-                        break;
-                    case "÷":
-                        if(placeHolder==0){
-                            updateDisplay(null,"didivdedByZero")
-                            numbersArray = [];
-                            currentIndex = 0;
-                            operatorsArray = [];
-                            return
-                        }else{
-                            ans /= placeHolder;
-                            break;
-                        }
-                    case null:
-                        console.log(operatorsArray[j])
-                        break;
-                    default:
-                        // do nothing
-                        console.log("something is wrong in switch(operator)||end of loop")
-                        break;
-                }
-            }
-        }
-        console.log(ans)
-
-        updateDisplay(ans,"equal")
-        resetData(ans)
+function glitchAnimation(invalidOperation){
+    if(invalidOperation){
+        displayPast.style.display = "none";
+        displayCurrent.style.display = "none";
+        glitch.style.display = "block";
+        allClear.classList.add("shake-infinite");
+    }else{
+        displayPast.style.display = "block";
+        displayCurrent.style.display = "block";
+        glitch.style.display = "none";
+        allClear.classList.remove("shake-infinite");
     }
-}
-
-function resetData(ans){
-    numbersArray = [];
-    currentIndex = 0;
-    numbersArray[0] = ans;
-    operatorsArray = [];
-}
-
-function updateArrays(){
-    if(numbersArray.length <= operatorsArray.length){
-        operatorsArray.pop();
-    }
-    numbersArray = numbersArray.map(str =>{
-        return Number(str)
-    });
-}
-
-function clearScreen(){
-    displayCurrent.textContent = "";
-    displayPast.textContent = "";
-    numbersArray = [];
-    operatorsArray = [];
-    currentIndex = 0;
 }
